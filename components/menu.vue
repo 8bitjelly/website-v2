d88512802befc6cf3efadd068f70f9c52c46fe2f
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2023-11-20 14:50:45 +0100

    Update menu.vue

--- components/menu.vue
+++ components/menu.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="h-16 py-4 ">
+    <div class="h-14 py-4 ">
         <div class="flex justify-center items-center gap-x-4">
             <NuxtLink to="#" class="text-lg font-semibold">{{ $t('menu.tech') }}</NuxtLink>
             <NuxtLink to="#" class="text-lg font-semibold">{{ $t('menu.blog') }}</NuxtLink>
-            <nuxt-img class="h-16" loading="lazy" src="/logo_black.svg" alt="8BitJelly"/>
+            <nuxt-img class="h-12" loading="lazy" src="/logo_black.svg" alt="8BitJelly"/>
             <NuxtLink to="#" class="text-lg font-semibold">{{ $t('menu.about') }}</NuxtLink>
             <NuxtLink to="#" class="text-lg font-semibold">{{ $t('menu.projects') }}</NuxtLink>
         </div>
